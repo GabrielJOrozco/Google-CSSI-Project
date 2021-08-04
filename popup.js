d50f738f0ee8a5527c72b236d58ea8cd019c97b1
GabrielJOrozco <88296273+GabrielJOrozco@users.noreply.github.com>
2021-08-04 19:21:39 -0400

    Add files via upload

--- popup.js
+++ popup.js
@@ -1,10 +1,42 @@
 console.log("Popup is running");
 let content = document.getElementById("sentiment");
+let toggleSwitch = document.getElementById("toggleSwitch");
+let buttonOn = false;
 console.log(content);
 
 document.addEventListener('DOMContentLoaded', () => {
-    let extension = chrome.extension.getBackgroundPage();
-    console.log(extension.sentiment.type);
-    content.innerHTML = extension.sentiment.type;
+    if (buttonOn) {
+        let extension = chrome.extension.getBackgroundPage();
+
+        let sentimentAnalysis = extension.sentiment.type;
+        let sentimentScore = extension.sentiment.score;
+
+        console.log(sentimentAnalysis);
+        console.log(sentimentScore);
+        content.innerHTML = sentimentAnalysis + ",            Score: " + sentimentScore;
+    }
 });
 
+
+toggleSwitch.onclick = () => {
+    console.log("clicked button");
+    if (buttonOn) {
+        buttonOn = false;
+        console.log("Button on");
+        chrome.browserAction.setBadgeText({text: 'ON'});
+        chrome.browserAction.setBadgeBackgroundColor({color: '#4688F1'});
+    } else {
+        buttonOn = true;
+        chrome.browserAction.setBadgeText({text: 'OFF'});
+        chrome.browserAction.setBadgeBackgroundColor({color: '#4688F1'});
+    }
+    /*if (buttonOn) {
+        console.log("Button on");
+        chrome.browserAction.setBadgeText({text: 'ON'});
+        chrome.browserAction.setBadgeBackgroundColor({color: '#4688F1'});
+    } else {
+        chrome.browserAction.setBadgeText({text: 'OFF'});
+        chrome.browserAction.setBadgeBackgroundColor({color: '#4688F1'});
+    } */
+    
+}
